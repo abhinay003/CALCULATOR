c28e44a673ddf7792606d5fddfb43b44bd4f4eb0
abhinay003 <abhiabhinay590@gmail.com>
2025-08-18 09:57:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,18 @@
-let firstnumber="";
-let secondnumber="";
-let operator="";
+let display = document.getElementById('display');
+function appendValue(value) {
+    display.value += value;
 
-function append(number){
-    if(operator===""){
-        firstnumber+=number;
-        document.getElementById('res').value=firstnumber;
-    }
-    else{
-        secondnumber+=number;
-        document.getElementById('res').value=firstnumber+operator+secondnumber;
-    }
-}
-function operate(op){
-    operator=op;
-    document.getElementById('res').value=firstnumber+operator
 }
 
+function clearDisplay(){
+    display.value= '';
+
+}
 function calculate(){
-    playsound();
-    let result;
-    switch(operator){
-        case '+':
-            result=parseInt(firstnumber)+parseInt(secondnumber);
-            break;
-            case '-':
-            result=parseInt(firstnumber)-parseInt(secondnumber);
-            break;
-            case '*':
-            result=parseInt(firstnumber)*parseInt(secondnumber);
-            break;
-            case '/':
-            result=parseInt(firstnumber)/parseInt(secondnumber);
-            break;
-            
-    }
-document.getElementById('res').value=result;
-}
-function clearresult(){
-    playsound();
-    firstnumber="";
-    secondnumber="";
-    operator="";
-    document.getElementById('res').value="";
-}
-let clicksound=new Audio("mouse-button-click-308449.mp3");
+    try{
+        display.value = eval (display.value);
 
-function playsound(){
-    clicksound.currentTime=0;
-    clicksound.play();
+    }catch {
+        display.value = "Error";
+    }
 }
